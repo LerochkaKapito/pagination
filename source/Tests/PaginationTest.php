5c986a6319ba811f8edbb60913d73016ec7b4720
Kilte Leichnam <nwotnbm@gmail.com>
2014-08-31 17:01:40 +0400

    PHPDoc updates

--- source/Tests/PaginationTest.php
+++ source/Tests/PaginationTest.php
@@ -3,6 +3,9 @@
 /**
  * Part of the Pagination
  *
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ *
  * @author  Kilte Leichnam <nwotnbm@gmail.com>
  * @package Pagination
  */
